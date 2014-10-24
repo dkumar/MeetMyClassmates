45bfefc05f337297226f76f28a0cff2320fb442f
Deepak Kumar <deepak9308@gmail.com>
2014-10-23 22:08:18 -0700

    [#81250838] Made devise pages use separate layout
    Added authentication from devise before showing any page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,19 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # Devise: Do not show any page except log in if not logged in
+  before_action :authenticate_user!
+
+  layout :layout_by_resource
+
+  protected
+  def layout_by_resource
+    if devise_controller?
+      'devise'
+    else
+      'application'
+    end
+  end
+
 end
