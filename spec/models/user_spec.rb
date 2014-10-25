676eaf7ff5bc63abdb66dc600c328384cf3c1a23
Deepak Kumar <deepak9308@gmail.com>
2014-10-24 21:16:44 -0700

    Added specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,86 @@
 require 'rails_helper'
 
 describe User do
-  it '' do
+  before :each do
+    User.delete_all
+    Studygroup.delete_all
+    Course.delete_all
+
+    @course = FactoryGirl.create(:course)
+    @user = FactoryGirl.create(:user)
+    @owner = FactoryGirl.create(:user)
   end
+
+  it 'enrolling in a valid class that user is not enrolled in' do
+    rtn_value = User.enroll_course(@user, @course.title)
+    expect(rtn_value).to eq(GlobalConstants::SUCCESS)
+  end
+
+  it 'attempting to enroll in invalid class' do
+    rtn_value = User.enroll_course(@user, '1')
+    expect(rtn_value).to eq(GlobalConstants::COURSE_NONEXISTENT)
+  end
+
+  it 'attempting to enroll in class user is already enrolled in' do
+    User.enroll_course(@user, @course.title)
+    rtn_value = User.enroll_course(@user, @course.title)
+    expect(rtn_value).to eq(GlobalConstants::USER_ALREADY_ENROLLED)
+  end
+
+  it 'user unenrolls in a course they are previously enrolled in' do
+    User.enroll_course(@user, @course.title)
+    rtn_value = User.unenroll_course(@user, @course.title)
+    expect(rtn_value).to eq(GlobalConstants::SUCCESS)
+  end
+
+  it 'user unenrolls in a class that doesn\t exist' do
+    rtn_value = User.unenroll_course(@user, '1')
+    expect(rtn_value).to eq(GlobalConstants::COURSE_NONEXISTENT)
+  end
+
+  it 'user unenrolls from a class that user is not enrolled in' do
+    rtn_value = User.unenroll_course(@user, @course.title)
+    expect(rtn_value).to eq(GlobalConstants::USER_NOT_ALREADY_ENROLLED)
+  end
+
+  it 'knows which courses he/she is enrolled in' do
+    User.enroll_course(@user, @course.title)
+    User.enroll_course(@user, '1')
+    rtn_value = User.list_courses(@user)
+    expect(rtn_value).to eq(@user.courses)
+  end
+
+"""
+  it 'joins studygroup that exists and user is not member of it' do
+    rtn_value = User.join_studygroup(@user, @studygroup.id)
+    expect(rtn_value).to eq(GlobalConstants::SUCCESS)
+  end
+
+  it 'joins invalid studygroup that user is not member of' do
+    rtn_value = User.join_studygroup(@user, 10)
+    expect(rtn_value).to eq(GlobalConstants::STUDYGROUP_DOES_NOT_EXIST)
+  end
+
+  it 'joins valid studygroup that user is member of' do
+    User.join_studygroup(@user, @studygroup.id)
+    rtn_value = User.join_studygroup(@user, @studygroup.id)
+    expect(rtn_value).to eq(GlobalConstants::USER_ALREADY_IN_STUDYGROUP)
+  end
+
+  it 'user leaves valid studygroup that he/she is a member of' do
+    User.join_studygroup(@user, @studygroup.id)
+    rtn_value = User.leave_studygroup(@user, @studygroup.id)
+    expect(rtn_value).to eq(GlobalConstants::SUCCESS)
+  end
+
+  it 'user leaves valid studygroup that he/she is NOT a member of' do
+    rtn_value = User.leave_studygroup(@user, @studygroup.id)
+    expect(rtn_value).to eq(GlobalConstants::USER_NOT_IN_STUDYGROUP)
+  end
+
+  it 'user leaves invalid studygroup' do
+    rtn_value = User.leave_studygroup(@user, @studygroup.id)
+    expect(rtn_value).to eq(GlobalConstants::STUDYGROUP_DOES_NOT_EXIST)
+  end
+"""
 end
--- spec/requests/user_requests.rb
+++ spec/requests/user_requests.rb
@@ -14,15 +14,18 @@ describe UsersController do
   end
 
   it 'enrolls in a course' do
-    post enroll_course_path, :course => @course.title
+    post enroll_course_path, :parameters => {:course => @course.title}
     visit user_show_path(@user)
     expect(page).to have_content(@course.title)
   end
 
   it 'unenrolls from a course' do
     post enroll_course_path, :course => @course.title
+    expect(page).to have_content(@course.title)
+
     post unenroll_course_path, :course => @course.title
-    get user_show_path(@user)
     expect(page).to have_no_content(@course.title)
   end
+
+
 end
