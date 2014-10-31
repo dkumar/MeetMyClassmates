819446e15e503b7cff0e36bb7e75e25b77fb2130
Lawrence Supian <lsupian1109@gmail.com>
2014-10-31 13:56:30 -0700

    devise gmail deliveries try 1

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -11,7 +11,7 @@ Devise.setup do |config|
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
   config.mailer_sender = 'lsupian1109@gmail.com'
-  config.action_mailer.perform_deliveries = true
+  #config.action_mailer.perform_deliveries = true
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
 
