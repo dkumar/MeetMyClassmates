3ab59d659e67a901e34ca51f0234c6407205fe1b
Deepak Kumar <deepak9308@gmail.com>
2014-11-04 22:35:00 -0800

    Implemented notifications in controller
    Changed users_controller to display notification based on result on query
    Moved code in user method to match similar methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,31 +1,67 @@
 class UsersController < ApplicationController
 
 	def show
-		if current_user
-			#@user = User.find(params[:id])
-			#render 'constant/banner'
-    else
-
-		end
 	end
 
 	def enroll_course
-    current_user.enroll_course(params[:course_name])
+    rtn_code = current_user.enroll_course(params[:course_name])
+
+    if rtn_code == GlobalConstants::COURSE_NONEXISTENT
+      flash[:error] = "Error: #{params[:course_name]} does not exist."
+    elsif rtn_code == GlobalConstants::USER_ALREADY_ENROLLED
+      flash[:error] = "Error: You are already enrolled in #{params[:course_name]}."
+    else
+      flash[:success] = "Success: You have successfully enrolled in #{params[:course_name]}."
+    end
+
     render :show
 	end
 
   def unenroll_course
-    current_user.unenroll_course(params[:course_name])
+    rtn_code = current_user.unenroll_course(params[:course_name])
+
+    if rtn_code == GlobalConstants::COURSE_NONEXISTENT
+      flash[:error] = "Error: #{params[:course_name]} does not exist."
+    elsif rtn_code == GlobalConstants::USER_ALREADY_ENROLLED
+      flash[:error] = "Error: You are already enrolled in #{params[:course_name]}."
+    else
+      flash[:success] = "Success: You have successfully unenrolled in #{params[:course_name]}."
+    end
+
     render :show
   end
 
   def join_studygroup
-    join_result = current_user.join_studygroup(params[:studygroup_id])
-    render json: {join_result: join_result}
+    rtn_code = current_user.join_studygroup(params[:studygroup_id])
+
+    if rtn_code == GlobalConstants::STUDYGROUP_DOES_NOT_EXIST
+      flash[:error] = "Error: Studygroup with id #{params[:studygroup_id]} does not exist."
+    elsif rtn_code == GlobalConstants::USER_NOT_ALREADY_ENROLLED
+      flash[:error] = "Error: You are not enrolled in the course that Studygroup #{params[:studygroup_id]} is associated with."
+    elsif rtn_code == GlobalConstants::USER_ALREADY_IN_STUDYGROUP
+      flash[:error] = "Error: You are already in Studygroup #{params[:studygroup_id]}."
+    else
+      flash[:success] = "Success: You have successfully joined Studygroup #{params[:studygroup_id]}."
+    end
+
+    # TODO: change to redirect to correct page (same for leave_studygroup)
+    # TODO: display studygroup and course name instead of id in notifications? (same for leave_studygroup)
+    render json: {join_result: rtn_code}
   end
 
   def leave_studygroups
-    leave_result = current_user.leave_studygroup(params[:studygroup_id])
-    render json: {leave_result: leave_result}
+    rtn_code = current_user.leave_studygroup(params[:studygroup_id])
+
+    if rtn_code == GlobalConstants::STUDYGROUP_DOES_NOT_EXIST
+      flash[:error] = "Error: Studygroup with id #{params[:studygroup_id]} does not exist."
+    elsif rtn_code == GlobalConstants::USER_NOT_ALREADY_ENROLLED
+      flash[:error] = "Error: You are not enrolled in the course that Studygroup #{params[:studygroup_id]} is associated with."
+    elsif rtn_code == GlobalConstants::USER_NOT_IN_STUDYGROUP
+      flash[:error] = "Error: You are not in Studygroup #{params[:studygroup_id]}."
+    else
+      flash[:success] = "Success: You have successfully joined Studygroup #{params[:studygroup_id]}."
+    end
+
+    render json: {leave_result: rtn_code}
   end
 end
