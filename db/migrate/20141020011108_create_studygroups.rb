e9c07ae9ac77981c239ab5c1247f9dfa9e71f5de
alice-wang <alcwng3@gmail.com>
2014-11-04 18:16:10 -0800

    fixed studygroup migration

--- db/migrate/20141020011108_create_studygroups.rb
+++ db/migrate/20141020011108_create_studygroups.rb
@@ -3,15 +3,23 @@ class CreateStudygroups < ActiveRecord::Migration
     create_table :studygroups do |t|
       t.belongs_to :course
       t.string :name
-      t.time :time
+      t.time :start_time
+      t.time :end_time
       t.date :date
       t.string :location
       t.integer :owner_id
       t.integer :minimum_size
       t.integer :maximum_size
+
       t.boolean :private
-      t.integer :recurring
-      t.string :tags
+      t.string :invited_users, array: true, default: []
+
+      t.boolean :recurring
+      t.integer :recurring_days, array: true, default: []
+      t.date :last_occurrence
+
+      t.string :tags, array: true, default: []
+
       t.boolean :unscheduled
 
       t.timestamps
