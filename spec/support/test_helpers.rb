c6bdc29bdd76ccf7b728f95651ddceab5af65aa0
Deepak Kumar <deepak9308@gmail.com>
2014-11-07 18:05:49 -0800

    Added tests for studygroup show page

--- spec/support/test_helpers.rb
+++ spec/support/test_helpers.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 module TestHelpers
   def login_user
     Capybara.current_session.driver.browser.clear_cookies
-    User.delete_all
     @user = User.create(email: 'email@berkeley.edu', password: 'password')
     @user.skip_confirmation!
     @user.save
@@ -11,6 +10,15 @@ module TestHelpers
     visit root_url
     fill_in 'user_email', :with => @user.email
     fill_in 'user_password', :with => @user.password
+    click_button 'Log In'
+  end
+
+  def login_owner
+    Capybara.current_session.driver.browser.clear_cookies
+
+    visit root_url
+    fill_in 'user_email', :with => @owner.email
+    fill_in 'user_password', :with => @owner.password
     click_button 'Log In'
   end
 end
