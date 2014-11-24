4eb3bedbd1040573c8ea35876d176aa48e4c5301
alice-wang <alcwng3@gmail.com>
2014-11-23 16:03:56 -0800

    Added more UI tests for studygroup show page

--- spec/requests/studygroup_show_spec.rb
+++ spec/requests/studygroup_show_spec.rb
@@ -27,6 +27,8 @@ describe 'show page' do
 
     @user.enroll_course(@course.title)
     @user.join_studygroup(@studygroup.id)
+    @user.join_studygroup(@unscheduled_studygroup.id)
+
 
     @non_member_user = create_user
   end
@@ -43,6 +45,12 @@ describe 'show page' do
     expect(page).to have_content('Success')
   end
 
+  it 'allows user to leave unscheduled studygroup' do
+    visit studygroup_show_path(@unscheduled_studygroup)
+    click_button 'Leave'
+    expect(page).to have_content('Success')
+  end
+
   it 'allows user to join studygroup' do
     visit studygroup_show_path(@studygroup)
     click_button 'Leave'
@@ -51,31 +59,51 @@ describe 'show page' do
     expect(page).to have_content('Success')
   end
 
+  it 'allows user to join an unscheduled studygroup' do
+    visit studygroup_show_path(@unscheduled_studygroup)
+    click_button 'Leave'
+    visit studygroup_show_path(@unscheduled_studygroup)
+    click_button 'Join'
+    expect(page).to have_content('Success')
+  end
+
+  it 'does not allow users to delete studygroup if not owner' do
+    visit studygroup_show_path(@studygroup)
+    page.should have_no_button('Delete')
+  end
+
+  it 'does not allow users to delete unscheduled studygroup if not owner' do
+    visit studygroup_show_path(@unscheduled_studygroup)
+    page.should have_no_button('Delete')
+  end
+
   it 'allows owner to delete studygroup' do
     login_user(@owner)
-
     visit studygroup_show_path(@studygroup)
     click_button 'Delete'
     expect(page).to have_content('Success')
   end
 
+  it 'allows owner to delete unscheduled studygroup' do
+    login_user(@owner)
+    visit studygroup_show_path(@unscheduled_studygroup)
+    click_button 'Delete'
+    expect(page).to have_content('Success')
+  end
+
   it 'allows user to post a message if user is a member' do
-    login_user(@user)
     visit studygroup_show_path(@studygroup)
     text = '123'
     fill_in 'message_body', with: text
     click_button 'Post Message'
-
     expect(page).to have_content(text)
   end
 
   it 'allows user to view messages even if user is not a member' do
-    login_user(@user)
     visit studygroup_show_path(@studygroup)
     text = 'abc'
     fill_in 'message_body', with: text
     click_button 'Post Message'
-
     login_user(@non_member_user)
     visit studygroup_show_path(@studygroup)
     expect(page).to have_content(text)
@@ -86,4 +114,16 @@ describe 'show page' do
     visit studygroup_show_path(@studygroup)
     page.should have_no_button('Post Message')
   end
+
+  it 'owner can view edit button' do
+    login_user(@owner)
+    visit studygroup_show_path(@unscheduled_studygroup)
+    page.should have_button('Edit')
+  end
+
+  it 'does not allow non owner to view edit button' do
+    visit studygroup_show_path(@unscheduled_studygroup)
+    page.should have_no_button('Edit')
+  end
+
 end
